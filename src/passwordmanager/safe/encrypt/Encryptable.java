c9dc6493253555de4206d334103b4337155ba997
naliuj <naliuj@users.noreplu.github.com>
2017-05-08 09:38:08 -0400

    files that deal with encryption now have their own package

--- src/passwordmanager/safe/encrypt/Encryptable.java
+++ src/passwordmanager/safe/encrypt/Encryptable.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package passwordmanager.safe;
+package passwordmanager.safe.encrypt;
 
 /**
  *
  * @author julian
  */
-public class DummyEncrypt {
+public interface Encryptable {
     
 }
