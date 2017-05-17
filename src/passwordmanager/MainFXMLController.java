df8d52aa08960c43bc7c7ef63550d2b09600c31d
naliuj <naliuj@users.noreplu.github.com>
2017-05-17 10:18:32 -0400

    removed unused imports

--- src/passwordmanager/MainFXMLController.java
+++ src/passwordmanager/MainFXMLController.java
@@ -5,28 +5,17 @@
  */
 package passwordmanager;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import passwordmanager.hash.HashFactory;
 import passwordmanager.hash.Hashable;
 import passwordmanager.safe.Entry;
